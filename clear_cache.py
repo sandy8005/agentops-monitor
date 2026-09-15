334d927d81bdd029e278d5814e36e8bfd655bb5e
Sandeep Barige <sandeep7212004@gmail.com>
2026-09-14 21:07:30 -0400

    Remove throwaway clear_cache utility

--- clear_cache.py
+++
@@ -1,9 +0,0 @@
-import os, psycopg2
-from dotenv import load_dotenv
-load_dotenv()
-c = psycopg2.connect(dbname=os.getenv("DB_NAME"), user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"), host=os.getenv("DB_HOST"), port=os.getenv("DB_PORT"))
-cur = c.cursor()
-cur.execute("DELETE FROM job_reqs_cache")
-print("cleared", cur.rowcount, "rows")
-c.commit()
